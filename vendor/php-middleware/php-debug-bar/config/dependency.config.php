2653908c4dbf33943c7be0fa15a186f89073d3ba
bootta <Bootta@yandex.ru>
2018-03-08 11:14:25 +0300

    blabla

--- vendor/php-middleware/php-debug-bar/config/dependency.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'factories' => [
-        PhpMiddleware\PhpDebugBar\PhpDebugBarMiddleware::class => PhpMiddleware\PhpDebugBar\PhpDebugBarMiddlewareFactory::class,
-        DebugBar\DataCollector\ConfigCollector::class => PhpMiddleware\PhpDebugBar\ConfigCollectorFactory::class,
-    ],
-];
