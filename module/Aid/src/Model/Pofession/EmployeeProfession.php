4df1edb998d070fdd0a542366f2622a448a1ab75
bootta <b.ahmedov@fabrikant.ru>
2018-04-06 23:37:51 +0300

    get error json-rps

---
+++ module/Aid/src/Model/Pofession/EmployeeProfession.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bootta
+ * Date: 05.04.18
+ * Time: 16:26
+ */
+
+class EmployeeProfession
+{
+	public $id_employee;
+	public $id_profession;
+	public $date_create;
+	public $is_deleted;
+
+
+	public function exchangeArray($data)
+	{
+		$this->id_profession  = (!empty($data['id_employee'])) ? $data['id_employee'] : null;
+		$this->id_profession  = (!empty($data['id_profession'])) ? $data['id_profession'] : null;
+		$this->date_create    = (!empty($data['date_create'])) ? $data['date_create'] : null;
+		$this->is_deleted     = (!empty($data['is_deleted'])) ? $data['is_deleted'] : '0';
+	}
+}
